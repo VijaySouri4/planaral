65cb03d292f4be6595dd35a1dbac4999dcde885b
Vijay Souri <44298217+VijaySouri4@users.noreply.github.com>
2023-10-28 12:57:59 -0400

    First commit; start to a long ass journey I guess.

---
+++ main.py
@@ -0,0 +1,9 @@
+from reactpy import component, html, run
+
+
+@component
+def App():
+    return html.h3("Wasup world, this is going to be my journal app!! Rejoice for I have started this ardous journey for everyone!! HAHAHAHAHAHAHAHAHA")
+
+
+run(App)
